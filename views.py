60c75acbb2b34a818c16c5a43a943beaf8ac2c40
sainiajits <sainiajits@gmail.com>
2022-08-08 17:30:38 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,51 +1,79 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User, auth
+from .models import Student
+from student.form import studentForm
 from django.contrib import messages
+from django.contrib.auth.models import User, auth 
 # Create your views here.
 
-def register(request):
-    if request.method=='POST':
-        first_name= request.POST['first_name']
+def home(request):
+    return render(request, 'index.html')
+
+def registration(request):
+    return render(request, 'registration.html')
+
+def instertData(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
         last_name = request.POST['last_name']
+        email = request.POST['email'] 
         username = request.POST['username']
-        email = request.POST['email']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
-        if password1==password2:
-            if User.objects.filter(username=username).exists():
-                print("Username is already exists...!")
-            elif User.objects.filter(email=email).exists():
-                 print("email is already exists...!")
+        if password1 == password2:
+            if Student.objects.filter(username=username).exists():
+                messages.info(request, 'Username allready exists...')
+                return redirect('registration')
+            elif Student.objects.filter(email=email).exists():
+                messages.info(request, 'Email allready exists...')
+                return redirect('registration')
             else:
-                user = User.objects.create_user(username=username,password=password1, email=email, first_name=first_name, last_name=last_name)
-                user.save()
-                print("user created...!")
-                return redirect('login')
-        else:
-            print("password not metching...!")
-
-        return redirect('/')
+                insrt = Student(first_name=first_name, last_name=last_name, email=email, username=username, password=password2)
+                insrt.save()
+                messages.success(request, 'User Submit successfully...!')
+                return redirect('all-records')
+        else:            
+            messages.error(request, 'Password does not match')
+            return redirect('registration')
     else:
-        return render(request, 'register.html')
+        return render(request, 'registration.html')
+
+def fetchData(request):
+    allRecords = Student.objects.all()
+    return render(request, 'student.html', {"records":allRecords})
+
+def editData(request, id):
+    edit = Student.objects.get(id=id)
+    return render(request, 'update.html', {"edit":edit})
+
+def updateData(request, id):
+    update = Student.objects.get(id=id)
+    form = studentForm(request.POST, instance=update)
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Record updated successfully...!')   
+        return render(request, 'update.html', {"edit":update})
     
+def deleteData(request, id):
+    delete = Student.objects.get(id=id)
+    delete.delete()
+    messages.success(request, 'Record Delete successfully...!')
+    return redirect('all-records')
+
 def login(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        
         user = auth.authenticate(username=username, password=password)
         
         if user is not None:
             auth.login(request, user)
             return redirect('/')
-        
         else:
-            messages.info(request, 'Invalide username or password')
+            messages.success(request, 'Incorrect Username or password')
             return redirect('login')
-        
     else:
         return render(request, 'login.html')
     
 def logout(request):
     auth.logout(request)
-    return redirect("/")
+    return redirect('/')
