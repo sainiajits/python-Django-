b1e23dd799d5f67b2d04ca42e62222d00127db3b
sainiajits <sainiajits@gmail.com>
2022-08-08 17:29:51 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from accounts import views
-
+from student import views
 
 urlpatterns = [
-    
-    path('register', views.register, name="register"),
-    path('login', views.login, name="login"),
-    path('login', views.login, name="login"),
+    path('', views.home, name='home'),
+    path('registration/', views.registration, name="registration"),
+    path('instertData/', views.instertData, name="instertData"),
+    path('all-records/', views.fetchData, name="all-records"),
+    path('editData/<int:id>', views.editData, name="editData"),
+    path('updateData/<int:id>', views.updateData, name="updateData"),
+    path('deleteData/<int:id>', views.deleteData, name="deleteData"),
+    path('login/', views.login, name="login"),
+    path('logout/', views.logout, name="logout"),
 ]
