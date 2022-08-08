eaae1f22922737409fb6c4ad5a1c35e3123d859e
sainiajits <sainiajits@gmail.com>
2022-08-08 17:28:15 +0530

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Student
 # Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name','username', 'email', 'password']
